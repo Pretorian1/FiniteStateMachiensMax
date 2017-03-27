db8a20ecd4e2508d65211db9977eb839c778f776
Max Koval <pretorian1@i.ua>
2017-03-27 13:00:19 +0300

    third commit

--- app/src/test/java/com/example/max/finitestatemachiensmax/MainActivityTest.java
+++ app/src/test/java/com/example/max/finitestatemachiensmax/MainActivityTest.java
@@ -1,44 +1,114 @@
 package com.example.max.finitestatemachiensmax;
 
 import android.os.Build;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.max.finitestatemachiensmax.Activities.MainActivity;
+import com.example.max.finitestatemachiensmax.Objects.FiniteMachineState;
 import com.google.firebase.FirebaseApp;
 
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowToast;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Max on 26.03.2017.
  */
+@FixMethodOrder(MethodSorters.DEFAULT)
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 
     private MainActivity activity;
 
+    private Button lockButton;
+
+    private Button unLockButton;
+
+    private Button lockx2Button;
+
+    private Button unLockx2Button;
+
+
+    TextView stateTextView;
+
+    TextView armedTextView;
+
+    TextView disarmedTextView;
+
+    private String [] statesArray;
+
+    FiniteMachineState finiteMachineState;
 
     @Before
     public void setup() {
-
         activity = Robolectric.setupActivity(MainActivity.class);
+        statesArray = activity.getResources().getStringArray(R.array.alarms_state);
+        stateTextView = (TextView) activity.findViewById(R.id.state_text_view);
+        armedTextView = (TextView) activity.findViewById(R.id.armed_text_view);
+        disarmedTextView = (TextView) activity.findViewById(R.id.disarmed_text_view);
+        lockButton = (Button) activity.findViewById(R.id.lock_button);
+        unLockButton = (Button) activity.findViewById(R.id.unlock_button);
+        lockx2Button = (Button) activity.findViewById(R.id.lockx2_button);
+        unLockx2Button = (Button) activity.findViewById(R.id.unlockx2_button);
+        finiteMachineState = FiniteMachineState.getFiniteMachineState(statesArray[2]);
+        finiteMachineState.setArmed(false);
     }
-
+    @Test
+    public void shouldDefineDefaultStateTextViewContent() {
+        assertTrue("TextView contains incorrect text", statesArray[2].equals(stateTextView.getText().toString()));
+    }
 
     @Test
-    public void validatedefaultStateTextViewContent() {
-        TextView mainText = (TextView) activity.findViewById(R.id.state_text_view);
-       // assertNotNull("TextView could not be found", mainText);
-        assertTrue("TextView contains incorrect text", "AlarmDisarmed_AllUnlocked".equals(mainText.getText().toString()));
-      //  assertNull(mainText.getText().toString());
+    public void shouldDefineAndChangeStateButtonLockClick(){
+        lockButton.performClick();
+        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("Lock pressed"));
+        assertEquals(statesArray[1], stateTextView.getText().toString());
+        assertEquals(View.INVISIBLE, armedTextView.getVisibility());
+        assertEquals(View.VISIBLE, disarmedTextView.getVisibility());
     }
+
+    @Test
+    public void shouldDefineAndChangeStateButtonLockx2Click(){
+        lockx2Button.performClick();
+        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("Lockx2 pressed"));
+        assertEquals(statesArray[0], stateTextView.getText().toString());
+        assertEquals(View.VISIBLE, armedTextView.getVisibility());
+        assertEquals(View.INVISIBLE, disarmedTextView.getVisibility());
+    }
+
+    @Test
+    public void shouldDefineAndChangeStateButtonUnLockClick(){
+        unLockButton.performClick();
+        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("UnLock pressed"));
+        assertEquals(statesArray[3], stateTextView.getText().toString());
+        assertEquals(View.INVISIBLE, armedTextView.getVisibility());
+        assertEquals(View.VISIBLE, disarmedTextView.getVisibility());
+    }
+
+    @Test
+    public void shouldDefineAndChangeStateButtonUnLockx2Click(){
+        unLockx2Button.performClick();
+        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("UnLockx2 pressed"));
+        assertEquals(statesArray[2], stateTextView.getText().toString());
+        assertEquals(View.INVISIBLE, armedTextView.getVisibility());
+        assertEquals(View.VISIBLE, disarmedTextView.getVisibility());
+    }
+
 }
--- app/src/test/java/com/example/max/finitestatemachiensmax/StateUtilsTest.java
+++ app/src/test/java/com/example/max/finitestatemachiensmax/StateUtilsTest.java
@@ -1,12 +1,134 @@
 package com.example.max.finitestatemachiensmax;
 
 import android.os.Build;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.max.finitestatemachiensmax.Activities.MainActivity;
+import com.example.max.finitestatemachiensmax.Objects.FiniteMachineState;
+import com.example.max.finitestatemachiensmax.Utils.StateUtils;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 @RunWith(RobolectricTestRunner.class)
 public class StateUtilsTest {
+
+    private MainActivity activity;
+
+    private String [] statesArray;
+
+    FiniteMachineState finiteMachineState;
+
+ //   @Mock
+    TextView stateTextView;
+  //  @Mock
+    TextView armedTextView;
+  //  @Mock
+    TextView disarmedTextView;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+        statesArray = activity.getResources().getStringArray(R.array.alarms_state);
+        stateTextView = (TextView) activity.findViewById(R.id.state_text_view);
+        armedTextView = (TextView) activity.findViewById(R.id.armed_text_view);
+        disarmedTextView = (TextView) activity.findViewById(R.id.disarmed_text_view);
+        finiteMachineState = FiniteMachineState.getFiniteMachineState(statesArray[2]);
+        finiteMachineState.setArmed(false);
+    }
+
+    /*  <item>AlarmArmed_AllLocked</item>
+        <item>AlarmDisarmed_AllLocke</item>
+        <item>AlarmDisarmed_AllUnlocked</item>
+        <item>AlarmDisarmed_DriverUnlocked</item>*/
+
+    @Test
+    public void shouldDefineLockButtonState(){
+       // when(finiteMachineState.setState(statesArray[1]).then;
+      /*  when(finiteMachineState.getState()).thenReturn(statesArray[1]);*/
+        finiteMachineState.setState(statesArray[2]);
+        StateUtils.defineLockButtonState(finiteMachineState,statesArray);
+        assertEquals(finiteMachineState.getState(),statesArray[1]);
+        assertFalse(finiteMachineState.isArmed());
+
+        finiteMachineState.setState(statesArray[0]);
+        finiteMachineState.setArmed(true);
+        StateUtils.defineLockButtonState(finiteMachineState,statesArray);
+        assertEquals(finiteMachineState.getState(),statesArray[0]);
+        assertTrue(finiteMachineState.isArmed());
+    }
+
+    @Test
+    public void shoulDefineLockx2ButtonState(){
+        finiteMachineState.setState(statesArray[1]);
+        StateUtils.defineLockx2ButtonState(finiteMachineState,statesArray);
+        assertEquals(finiteMachineState.getState(),statesArray[0]);
+        assertTrue(finiteMachineState.isArmed());
+    }
+
+    @Test
+    public void shouldefineUnLockButtonState(){
+        finiteMachineState.setState(statesArray[1]);
+        StateUtils.defineUnLockButtonState(finiteMachineState,statesArray);
+        assertEquals(finiteMachineState.getState(),statesArray[3]);
+        assertFalse(finiteMachineState.isArmed());
+
+        finiteMachineState.setState(statesArray[2]);
+        finiteMachineState.setArmed(false);
+        StateUtils.defineUnLockButtonState(finiteMachineState,statesArray);
+        assertEquals(finiteMachineState.getState(),statesArray[2]);
+        assertFalse(finiteMachineState.isArmed());
+    }
+
+    @Test
+    public void shoulDefineUnLockx2ButtonState(){
+        finiteMachineState.setState(statesArray[2]);
+        StateUtils.defineUnLockx2ButtonState(finiteMachineState,statesArray);
+        assertEquals(finiteMachineState.getState(),statesArray[2]);
+        assertFalse(finiteMachineState.isArmed());
+
+        finiteMachineState.setState(statesArray[3]);
+        finiteMachineState.setArmed(false);
+        StateUtils.defineUnLockx2ButtonState(finiteMachineState,statesArray);
+        assertEquals(finiteMachineState.getState(),statesArray[3]);
+        assertFalse(finiteMachineState.isArmed());
+
+    }
+
+    @Test
+    public void shouldSetStateDependentViews() {
+        finiteMachineState.setState(statesArray[3]);
+        finiteMachineState.setArmed(false);
+        StateUtils.setStateDependentViews(finiteMachineState, stateTextView,
+                armedTextView, disarmedTextView);
+        assertEquals(finiteMachineState.getState(), stateTextView.getText().toString());
+        assertEquals(View.INVISIBLE, armedTextView.getVisibility());
+        assertEquals(View.VISIBLE, disarmedTextView.getVisibility());
+
+        finiteMachineState.setState(statesArray[0]);
+        finiteMachineState.setArmed(true);
+        StateUtils.setStateDependentViews(finiteMachineState, stateTextView,
+                armedTextView, disarmedTextView);
+        assertEquals(finiteMachineState.getState(), stateTextView.getText().toString());
+        assertEquals(View.VISIBLE, armedTextView.getVisibility());
+        assertEquals(View.INVISIBLE, disarmedTextView.getVisibility());
+    }
+
+
+
+
 }
